6dc81e9b1acd237dd812cfdab0773c27ab078a23
wankhaira25-pixel <wankhaira25@gmail.com>
2025-09-29 05:36:19 +0000

    “done”

---
+++ Part1/ex04.py
@@ -0,0 +1,15 @@
+num1 = 67
+num2 = 7
+
+num3 = int("25")
+num4 = int("19")
+
+print(num1/num2)
+print(num3/num4)
+
+#use this to turn anything into string
+#str()
+#use this to turn anything into integer
+#int()
+#use this to turn anything into float (decimal number)
+#float()
